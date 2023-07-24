0151e69c5eb1e439648c06624f601760f1ad29cf
KHARBAQOssama <ossamakharbaq4@gmail.com>
2023-07-24 18:07:26 +0200

    trying to organize the folders and made a test sign static pages which are not official

--- RovingRahal-frontend/src/router/index.js
+++ RovingRahal-frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/user/Home.vue';
+import Sign from '../views/Sign.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +8,13 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path:'/sign',
+      name :'sign',
+      component: Sign
+    },
   ]
 })
 
