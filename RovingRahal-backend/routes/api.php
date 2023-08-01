160accf2f1aa8b7d7206bc270a6f11d1f9a00c5c
KHARBAQOssama <ossamakharbaq4@gmail.com>
2023-08-01 12:35:36 +0200

    creation d'un controller auth controller , make a modif in the config/auth.php file changing the guard , changing the name of the db in .env.example from laravel to roving rahal , define the routes for the euthentication

--- RovingRahal-backend/routes/api.php
+++ RovingRahal-backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,15 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::group([
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 });
