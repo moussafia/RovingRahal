d125d0f125221f3e123be132c975fdbaa5bfcc0d
saad moumou <saad.moumou.coder@gmail.com>
2023-08-08 17:09:38 +0100

    add page main and im working partie post

--- RovingRahal-frontend/src/views/Home.vue
+++ RovingRahal-frontend/src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
+  <div class="flex">
     <Sidebar></Sidebar>
+    <Main></Main>
   </div>
 </template>
 
 <script>
 import Sidebar from '../components/globalComponents/Sidebar.vue';
+import Main from '../components/Main.vue';
 export default {
     components:{
         Sidebar,
+        Main,
     }
 }
 </script>
