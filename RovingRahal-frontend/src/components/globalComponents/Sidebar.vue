bcaf812fbf0feef7d8508018c66495c0b32de6ae
KHARBAQOssama <ossamakharbaq4@gmail.com>
2023-08-11 18:05:27 +0200

    navigation et aussi les form de authentication

--- RovingRahal-frontend/src/components/globalComponents/Sidebar.vue
+++ RovingRahal-frontend/src/components/globalComponents/Sidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="flex flex-col px-6">
+  <aside class="flex flex-col p-6">
     <ul class="mx-auto">
         <li @click="navItemClicked($event)" class="flex text active">
                 <svg class="me-4" xmlns="http://www.w3.org/2000/svg" id="Outline" viewBox="0 0 24 24" width="512" height="512"><path d="M23.121,9.069,15.536,1.483a5.008,5.008,0,0,0-7.072,0L.879,9.069A2.978,2.978,0,0,0,0,11.19v9.817a3,3,0,0,0,3,3H21a3,3,0,0,0,3-3V11.19A2.978,2.978,0,0,0,23.121,9.069ZM15,22.007H9V18.073a3,3,0,0,1,6,0Zm7-1a1,1,0,0,1-1,1H17V18.073a5,5,0,0,0-10,0v3.934H3a1,1,0,0,1-1-1V11.19a1.008,1.008,0,0,1,.293-.707L9.878,2.9a3.008,3.008,0,0,1,4.244,0l7.585,7.586A1.008,1.008,0,0,1,22,11.19Z"/></svg>
@@ -59,7 +59,6 @@ aside{
     height: 100vh;
     width: 370px;
     /* 630 370 */
-    padding-top: 100px;
     border-right: 1px solid var(--dark-color);
 }
 aside > ul > li{
