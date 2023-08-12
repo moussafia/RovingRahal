fb38851a03cff2f6ca4545dbf669d4415be60d40
moussafia <moussafia.mohammed1999@gmail.com>
2023-08-12 19:37:39 +0100

    je n'ajoutre rien

--- RovingRahal-frontend/src/router/index.js
+++ RovingRahal-frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue';
+import createBlogs from '../views/Blog.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Home
-    },
+    },{
+      path:'/createBlogs',
+      name: 'createBlogs',
+      component: createBlogs
+    }
   ]
 })
 
