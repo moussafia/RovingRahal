6176d59a0aade9d336b5ad29f98442aa6a115501
saad moumou <saad.moumou.coder@gmail.com>
2023-08-27 15:29:57 +0100

    fix structure page home

---
+++ RovingRahal-frontend/src/components/RightSideBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="right-sidebar">
+        <p class="bg-slate-200">Test</p>
+    </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.right-sidebar{
+    flex-basis: 25%;
+    position: sticky;
+    top: 30px;
+    align-self: flex-start;
+}
+</style>
--- RovingRahal-frontend/src/views/Home.vue
+++ RovingRahal-frontend/src/views/Home.vue
@@ -1,22 +1,26 @@
 <template>
   <navbar></navbar>
   <div class="flex">
-    <Sidebar></Sidebar>
+    <LeftSidebar></LeftSidebar>
     <Main></Main>
+    <RightSideBar></RightSideBar>
   </div>
 </template>
 
 <script>
 import Navbar from '../components/globalComponents/Navbar.vue';
-import Sidebar from '../components/globalComponents/Sidebar.vue';
+import LeftSidebar from '../components/globalComponents/LeftSidebar.vue';
 import Main from '../components/Main.vue';
+import RightSideBar from '../components/RightSideBar.vue';
+// import Container from '../components/Container.vue';
 
 export default {
     components:{
       Navbar,
-      Sidebar,
+      LeftSidebar,
       Main,
-    }
+      RightSideBar,
+    },
 }
 </script>
 
